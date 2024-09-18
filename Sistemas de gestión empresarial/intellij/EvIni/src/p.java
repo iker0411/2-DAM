9852a12915d7e061ff5db224ed2ad2e975ac5890
iker0411 <145666360+iker0411@users.noreply.github.com>
2024-09-18 18:38:57 +0200

    subir carpetas y proyecto

---
+++ Sistemas de gestión empresarial/intellij/EvIni/src/p.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class p {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numEnt;
+        System.out.println("Dime el número que hay de cocodrilos");
+        do {
+            numEnt = Integer.parseInt(sc.next());
+            if (numEnt < 1 || numEnt > 50){
+                System.out.println("No puede ser inferior a 1 ni susperior a 50");
+            }
+        }while (numEnt < 1 || numEnt > 50);
+
+
+    }
+}
