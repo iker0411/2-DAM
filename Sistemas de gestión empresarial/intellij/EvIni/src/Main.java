4fe7c4182fbdb7b4567a1d74561ea19b750bd5ab
iker0411 <imenpal2611@g.educaand.es>
2024-09-19 08:59:48 +0200

    Medio acabado, falta las vocales.

--- Sistemas de gestión empresarial/intellij/EvIni/src/Main.java
+++ Sistemas de gestión empresarial/intellij/EvIni/src/Main.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
 
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numEnt;
+        String nombre;
+        int cocodriloLlorando = 0;
+
+        String[] vocales = {"a", "e", "i", "o", "u"};
         System.out.println("Dime el número que hay de cocodrilos:");
         do {
-            numEnt = Integer.parseInt(sc.nextLine().toLowerCase());
+            numEnt = Integer.parseInt(sc.nextLine());
             if (numEnt < 1 || numEnt > 50) {
                 System.out.println("No puede ser inferior a 1 ni superior a 50");
             }
         } while (numEnt < 1 || numEnt > 50);
-pp
+        for (int i = 0; i < numEnt; i++) {
+            System.out.println("Dime el nombre del cocodrilo: ");
+            nombre = sc.nextLine().toLowerCase();
+            if (nombre.length() > 10) {
+                System.out.println("No puede ser superior a 10 letras");
+            }
+        }
+
     }
 }
