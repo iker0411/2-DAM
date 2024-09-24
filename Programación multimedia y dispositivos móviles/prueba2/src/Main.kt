74c00e894d8f5444f5c3569394771836a3c3b686
Iker <imenpal2611@g.educaand.es>
2024-09-24 10:18:38 +0200

    nuevo kotlin2

--- Programación multimedia y dispositivos móviles/prueba2/src/Main.kt
+++ Programación multimedia y dispositivos móviles/prueba2/src/Main.kt
@@ -1,5 +1,15 @@
+import java.util.*
+
+
 fun main(args: Array<String>) {
+    feedTheFish()
+    println("Argumentos de entrada al main: ")
+    args.forEach {
+        println("$it, ")
+    }
+    println("\n-----------------------------------")
     arrays()
+    println("\n-----------------------------------")
     var x = 2
     val fixed: Float = 5f
     x = x.times(40)
@@ -29,20 +39,74 @@ fun main(args: Array<String>) {
         println("hola $it, mi nombre tiene ${it.length} letras.")
     } ?: println("El nombre es nulo")
 }
-fun arrays(){
+
+
+fun arrays() {
     val school = listOf("mackerel", "trout", "halibut")
 
     println("Los elementos de una lista son: ")
-    school.forEach{
+    school.forEach {
         println("$it, ")
     }
 
-    val  myList = mutableListOf("tuna", "salmon", "shark")
-    myList.add(myList.size -1, "sardina")
+    val myList = mutableListOf("tuna", "salmon", "shark")
+    myList.add(myList.size - 1, "sardina")
     myList.add(myList.lastIndex, "boquerones")
     println(myList)
-}
+    for ((index, element) in myList.withIndex()) {
+        when {
+            index == myList.lastIndex - 1 -> print("y $element y ")
+            index == myList.lastIndex -> print("$element, ")
+            else -> print("$element. ")
+        }
+    }
 
+    println(sumaEnteras(3,8))
+
+    val temperatura = 10
+    val  isHot = if (temperatura > 50 ) true else false
+    println(isHot)
+    val temperature = 10
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+    println(message)
+}
+ fun sumaEnteras(a : Int, b : Int) : Int{
+    return a+b
+ }
 fun sayhello(): String {
     return "Hello World!"
+}
+
+/*fun feedTheFish() {
+    val day = randomDay()
+    val food =
+    println ("Today is $day and the fish eat $food")
+}*/
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+fun fishFood (day : String) : String {
+    var food = ""
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Tuesday" -> food = "pellets"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Saturday" -> food = "lettuce"
+        "Sunday" -> food = "plankton"
+    }
+    return food
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+
+    println ("Today is $day and the fish eat $food")
+}
+fun swim(speed: String = "fast") {
+    println("swimming $speed")
 }
