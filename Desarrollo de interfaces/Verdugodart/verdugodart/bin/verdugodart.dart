ee90c54c8efbad66277e5b8d2733c7e58a097137
iker0411 <145666360+iker0411@users.noreply.github.com>
2024-09-25 12:41:32 +0200

    dart verdugo

---
+++ Desarrollo de interfaces/Verdugodart/verdugodart/bin/verdugodart.dart
@@ -0,0 +1,6 @@
+import 'package:verdugodart/verdugodart.dart' as verdugodart;
+
+void main(List<String> arguments) {
+
+  print('Hello world: ${verdugodart.calculate()}!');
+}
---
+++ Desarrollo de interfaces/Verdugodart/verdugodart/test/verdugodart_test.dart
@@ -0,0 +1,8 @@
+import 'package:verdugodart/verdugodart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
