5bbb566074df9a5ba103cf44dfe225ce38afb694
iker0411 <145666360+iker0411@users.noreply.github.com>
2024-10-03 14:42:53 +0200

    s

--- Desarrollo de interfaces/Null Safety/ejer1/bin/ejer1.dart
+++
@@ -1,5 +0,0 @@
-import 'package:ejer1/ejer1.dart' as ejer1;
-
-void main(List<String> arguments) {
-  print('Hello world: ${ejer1.calculate()}!');
-}
--- Desarrollo de interfaces/Null Safety/ejer1/test/ejer1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ejer1/ejer1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
